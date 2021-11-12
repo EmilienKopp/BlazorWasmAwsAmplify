036c38444f2154a708254981e667663a959a76e6
Emilien Kopp <emilien.kopp@outlook.com>
2021-11-12 22:21:13 +0900

    Added POST API call

---
+++ Models/Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWasmAwsAmplify.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string StartLocation { get; set; }
+
+        [Required]
+        public string EndLocation { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BlazorWasmAwsAmplify.Models
+{
+    public class User
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+
+    }
+}
