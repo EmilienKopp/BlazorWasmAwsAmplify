1a4c07ab1e568f4de3320f04d62123cec6aa8966
Emilien Kopp <emilien.kopp@outlook.com>
2021-11-12 23:27:21 +0900

    Fixed null string causing issue with DynamoDB

--- Models/User.cs
+++ Models/User.cs
@@ -10,13 +10,13 @@ namespace BlazorWasmAwsAmplify.Models
     public class User
     {
         [Required]
-        public string Name { get; set; }
+        public string Name { get; set; } = "";
         [Required]
-        public string City { get; set; }
+        public string City { get; set; } = "";
         [Required]
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string Address { get; set; }
+        public string Email { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public string Address { get; set; } = "";
 
 
     }
